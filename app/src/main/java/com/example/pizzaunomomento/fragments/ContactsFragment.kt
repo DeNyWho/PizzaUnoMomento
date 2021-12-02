9097c1aa9c758b238850601d42debc45e0b9eb20
Denyssdss <denis.akhunov00@mail.ru>
2021-12-03 00:37:04 +0500

    update

--- app/src/main/java/com/example/pizzaunomomento/fragments/ContactsFragment.kt
+++ app/src/main/java/com/example/pizzaunomomento/fragments/ContactsFragment.kt
@@ -1,23 +1,19 @@
 package com.example.pizzaunomomento.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.pizzaunomomento.R
 
-class MainScreenNext : Fragment() {
-
+class ContactsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_main_screen_next, container, false)
 
-        return view
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
     }
-
-
 }
--- app/src/main/java/com/example/pizzaunomomento/fragments/cart.kt
+++ app/src/main/java/com/example/pizzaunomomento/fragments/cart.kt
@@ -1,20 +1,76 @@
 package com.example.pizzaunomomento.fragments
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import androidx.appcompat.widget.AppCompatSpinner
 import com.example.pizzaunomomento.R
+import com.tiper.MaterialSpinner
 
 class Cart : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+        val appCompatSpinner = view.findViewById<AppCompatSpinner>(R.id.appCompatSpinner)
+
+        ArrayAdapter.createFromResource(requireContext(), R.array.planets_array, android.R.layout.simple_spinner_item).let {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            appCompatSpinner.adapter = it
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        return view
+    }
+    private fun showDialogTime() {
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.cart_time)
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+    }
+    private fun showDialogScores() {
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.scores)
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+    }
+
+    private fun showDialogPromocode() {
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.promocode)
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
     }
 
 }
