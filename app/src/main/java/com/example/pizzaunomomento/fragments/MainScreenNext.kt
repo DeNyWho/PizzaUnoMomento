81d36cde1931a4f51927c8ef602da21c62301902
Denyssdss <denis.akhunov00@mail.ru>
2021-12-03 00:37:04 +0500

    update

---
+++ app/src/main/java/com/example/pizzaunomomento/fragments/MainScreenNext.kt
@@ -0,0 +1,26 @@
+package com.example.pizzaunomomento.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.pizzaunomomento.R
+import com.ruslankishai.unmaterialtab.tabs.RoundTabLayout
+
+class MainScreenNext : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main_screen_next, container, false)
+
+        return view
+    }
+
+
+}
